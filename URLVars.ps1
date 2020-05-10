2ee0100864028672003ca13f273cb67f190a6fb1
Marwan <marwan@automne.me>
2020-05-10 21:53:23 +0200

    Updating Version of MPC, LAV & SP

--- URLVars.ps1
+++ URLVars.ps1
@@ -1,5 +1,5 @@
-$Global:mpc = "https://freefr.dl.sourceforge.net/project/mpcbe/MPC-BE/Nightly%20Builds%20%28from%20svn%20trunk%29/1.5.3%20%28build%204322%29%20beta/MPC-BE.1.5.3.4322.x64.7z"
+$Global:mpc = "https://liquidtelecom.dl.sourceforge.net/project/mpcbe/MPC-BE/Nightly%20Builds%20%28from%20svn%20trunk%29/1.5.5%20%28build%205274%29%20beta/MPC-BE.1.5.5.5274.x86.7z"
 $Global:mad = "http://madshi.net/madVR.zip"
-$Global:lav = "https://github.com/Nevcairiel/LAVFilters/releases/download/0.73.1/LAVFilters-0.73.1-x64.zip"
+$Global:lav = "https://github.com/Nevcairiel/LAVFilters/releases/download/0.74.1/LAVFilters-0.74.1-x64.zip"
 $Global:xys = "https://github.com/Cyberbeing/xy-VSFilter/releases/download/3.1.0.752/XySubFilter_3.1.0.752_x64.zip"
-$Global:syn = "https://github.com/Syncplay/syncplay/releases/download/v1.6.2/Syncplay_1.6.2_Portable.zip"
+$Global:syn = "https://github.com/Syncplay/syncplay/releases/download/v1.6.4a/Syncplay_1.6.4a_Portable.zip"
